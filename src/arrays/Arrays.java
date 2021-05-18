c4f4e1f4e7f4d939113264a039bf30570e34644e
Mihai <kubikweg@gmail.com>
2021-05-18 12:25:58 +0300

    feature: sumArray and productArray

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -7,5 +7,23 @@ public class Arrays {
             System.out.println(arr[i]);
         }
     }
+    //Calculate the sum of elements from an array
+    public static int sumArray(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum +=arr[i];
+        }
+        return sum;
+    }
+    //Product of elements from an array
+    public static int productArray(int[] arr){
+        int product = 1;
+        for (int i = 0; i <arr.length ; i++) {
+            product*=arr[i];
+
+        }
+        return product;
+    }
+
 
 }
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int [] array = {10, 11, 100, 999};
-        displayArray(array);
+        //displayArray(array);
+        System.out.println(sumArray(array));
+        System.out.println(productArray(array));
     }
 }
