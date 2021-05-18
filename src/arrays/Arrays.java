3424a379dd4dc52c77e56fd58b644945ca1c8382
Mihai <kubikweg@gmail.com>
2021-05-18 12:16:51 +0300

    Initial commit

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Arrays {
+    //Method which display the elements of an array
+    public static void displayArray(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
---
+++ src/arrays/Main.java
@@ -0,0 +1,10 @@
+package arrays;
+import static arrays.Arrays.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] array = {10, 11, 100, 999};
+        displayArray(array);
+    }
+}
