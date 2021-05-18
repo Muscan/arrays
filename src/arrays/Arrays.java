b361fce913f15bb18898edc4513fdfe32cdb1c06
Mihai <kubikweg@gmail.com>
2021-05-18 13:03:01 +0300

    feature: prime, max, even numbers from an array

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -24,6 +24,70 @@ public class Arrays {
         }
         return product;
     }
+    //Product of even numbers
+    public static int productEvenArray(int[] arr){
+        int productEven = 1;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]%2 == 0)
+                productEven *=arr[i];
+        }
+        return productEven;
+    }
+    //Return number of even numbers
+    public static int evenNrArray(int[] arr){
+        int numberOfEven = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]%2 == 0)
+                numberOfEven++;
+        }
+        return numberOfEven;
+    }
+
+    //Return the max number from an array
+    public static int maxNrArray(int[] arr){
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max)
+                max = arr[i];
+            /**
+             * If a number FROM THE ARRAY  is GREATER than max, max takes that number's value
+             * This means that while going through the array, max always keeps the current max value
+             */
+        }
+        return max;
+    }
+    public static int maxEven(int[] arr){
+        int evenMax = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]>evenMax && arr[i]%2 == 0)
+                evenMax = arr[i];
+        }
+        return evenMax;
+    }
+    //Sum of negative numbers
+    public static int sumNegative(int[] arr){
+        int sum = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]<0)
+                sum += arr[i];
+        }
+        return sum;
+    }
+
+    //Prime numbers from an array
+    public static int primeNumbers(int[] arr){
+        int prime = 0;//the number of prime numbers in the arrau
+        for (int i = 0; i < arr.length; i++) {//we go through the whole array
+            boolean isPrime = true;// we keep this to know if a number is prime. If it is the values will remain true, otherwise false
+            for (int j = 2; j < arr[i]/2; j++) {// j goes from 2 to the half of the number. If the number (arr[i]) divides with j, it means it is not prime as it has multiple divisors
+                if(arr[i]%j == 0)//if it (arr[i]) has divisors
+                    isPrime = false;//it is not prime
+            }
+            if(isPrime)//if the number is prime
+                prime++;//the number of primes increases
+        }
+        return prime;
+    }
 
 
 }
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -8,5 +8,11 @@ public class Main {
         //displayArray(array);
         System.out.println(sumArray(array));
         System.out.println(productArray(array));
+        System.out.println(productEvenArray(array));
+        System.out.println("Total nr. of even numbers. is " + evenNrArray(array));
+        System.out.println("Max nr. is " + maxNrArray(array));
+        System.out.println("Max even is " + maxEven(array));
+        System.out.println("Sum of negative numbers is " + sumNegative(array));
+        System.out.println("Prime numbers are " + primeNumbers(array));
     }
 }
