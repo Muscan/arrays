c6c1c70946f7848b2bb71b197bd5f05474bf0ce2
Mihai <kubikweg@gmail.com>
2021-05-20 14:03:34 +0300

    feature: nr. of even values, absolut values, sum of even elements, paris of Array, minMax values of Array

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -7,6 +7,7 @@ public class Arrays {
             System.out.println(arr[i]);
         }
     }
+
     //Calculate the sum of elements from an array
     public static int sumArray(int[] arr){
         int sum = 0;
@@ -15,6 +16,7 @@ public class Arrays {
         }
         return sum;
     }
+
     //Product of elements from an array
     public static int productArray(int[] arr){
         int product = 1;
@@ -24,6 +26,7 @@ public class Arrays {
         }
         return product;
     }
+
     //Product of even numbers
     public static int productEvenArray(int[] arr){
         int productEven = 1;
@@ -33,6 +36,7 @@ public class Arrays {
         }
         return productEven;
     }
+
     //Return number of even numbers
     public static int evenNrArray(int[] arr){
         int numberOfEven = 0;
@@ -64,6 +68,7 @@ public class Arrays {
         }
         return evenMax;
     }
+
     //Sum of negative numbers
     public static int sumNegative(int[] arr){
         int sum = 0;
@@ -76,7 +81,7 @@ public class Arrays {
 
     //Prime numbers from an array
     public static int primeNumbers(int[] arr){
-        int prime = 0;//the number of prime numbers in the arrau
+        int prime = 0;//the number of prime numbers in the array
         for (int i = 0; i < arr.length; i++) {//we go through the whole array
             boolean isPrime = true;// we keep this to know if a number is prime. If it is the values will remain true, otherwise false
             for (int j = 2; j < arr[i]/2; j++) {// j goes from 2 to the half of the number. If the number (arr[i]) divides with j, it means it is not prime as it has multiple divisors
@@ -89,5 +94,100 @@ public class Arrays {
         return prime;
     }
 
+    //Diff in abs. value between total nr. of even and odd numbers
+    public static int nrValEven(int[] arr){
+        int countEven = 0;
+        for (int i = 0; i <arr.length; i++) {
+            if (arr[i]%2 == 0)
+                countEven++;
+        }
+        return countEven;
+    }
+
+    public static int nrValOdd(int[] arr){
+        int countOdd = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]%2 == 1)
+                countOdd++;
+
+        }
+        return countOdd;
+    }
+
+    public static int absValue(int[] arr){
+        return Math.abs(nrValEven(arr) - nrValOdd(arr));
+    }
+
+    //Display the elements which are multiples of the last element
+    public static int multiplesLastElement(int[] arr){
+        //ex: multiples of 3 are 0, 3, 6, 9, 12
+        int lastElementOfArray = arr[arr.length-1];
+        int multiplesCounter = 0;
+        for (int i = 0; i <arr.length-1; i++) {
+            if (arr[i]%lastElementOfArray == 0)
+                multiplesCounter++;
+        }
+        return multiplesCounter;
+    }
+
+    //Display pairs of: first, last, second last-1, etc.
+    public static void pairsOfArray(int[] myArray){
+
+        for (int i = 0, j=myArray.length-1; i<j; i++, j--) {
+            System.out.println("Pairs: "+"("+ +myArray[i]+", "+myArray[j]+")");
+        }
+    }
+
+    //Max and min element from an array
+    public static int minValueOfArray(int[] arr){
+        //suppose that the min is it`s first index
+        int minValue = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minValue)
+                minValue = arr[i];
+        }
+        return minValue;
+    }
+
+    public static int maxValueOfArray(int[] arr){
+        int maxValue = arr[0];
+        for (int i = 0; i <arr.length; i++) {
+            if (arr[i] > maxValue)
+                maxValue = arr[i];
+        }
+        return maxValue;
+    }
+
+    public static String minMaxValuesOfArray(int[] arr){
+
+        return "The minimum value from the array is " +minValueOfArray(arr)+" , and the max value is "+maxNrArray(arr);
+    }
+
+    //Calculate the sum between first and last even element of an array
+    public static int positionFirstEvenElement(int[] arr){
+
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]%2 == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //position of the last even Number
+    public static int positionLastEvenElement(int[] arr){
+        for (int i = arr.length-1; i >=0; i--) {
+            if (arr[i]%2 == 0)
+                return i;
+        }
+        return -1;
+    }
+    public static int sumEvenElements(int[] arr){
+        int sum = 0;
+        for (int i = positionFirstEvenElement(arr); i <= positionLastEvenElement(arr) ; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 
 }
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -3,16 +3,22 @@ import static arrays.Arrays.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int [] array = {10, 11, 100, 999};
-        //displayArray(array);
-        System.out.println(sumArray(array));
-        System.out.println(productArray(array));
-        System.out.println(productEvenArray(array));
-        System.out.println("Total nr. of even numbers. is " + evenNrArray(array));
-        System.out.println("Max nr. is " + maxNrArray(array));
-        System.out.println("Max even is " + maxEven(array));
-        System.out.println("Sum of negative numbers is " + sumNegative(array));
-        System.out.println("Prime numbers are " + primeNumbers(array));
+
+        int [] array = {10, 20, 330, 77, 12, 32, 56, 6, -1111, 66, 88, 99, 45, 24, 56778, 34534, 7890, -1231, 10};
+
+        System.out.println("Sum of the array is: " + sumArray(array));
+        System.out.println("Product of the array is: " + productArray(array));
+        System.out.println("Product of the even numbers is: " + productEvenArray(array));
+        System.out.println("Total nr. of even numbers. is: " + evenNrArray(array));
+        System.out.println("Max nr. is: " + maxNrArray(array));
+        System.out.println("Max even is: " + maxEven(array));
+        System.out.println("Sum of negative numbers is: " + sumNegative(array));
+        System.out.println("Prime numbers are: " + primeNumbers(array));
+        System.out.println("Nr. of even values " + nrValEven(array) + " and the nr. of odd values: " + nrValOdd(array));
+        System.out.println("Absolut value of even minus odd numbers is: " + absValue(array));
+        System.out.println("Multiples of the last element are: " + multiplesLastElement(array));
+        pairsOfArray(array);
+        minMaxValuesOfArray(array);
+        System.out.println("Sum of even elements is: "+sumEvenElements(array));
     }
 }
